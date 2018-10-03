5f8ec7dc4a9ea8de7dfd6fff1996334585a8f02a
abdalla maged <amaged@softmills.com>
2018-10-03 16:27:43 +0200

    bottom bar set phase 3

--- app/src/main/java/com/example/softmills/phlog/ui/MainActivity.java
+++ app/src/main/java/com/example/softmills/phlog/ui/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.softmills.phlog.ui;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.softmills.phlog.R;
 import com.example.softmills.phlog.base.BaseActivity;
@@ -16,9 +19,10 @@ import com.example.softmills.phlog.ui.photographerprofile.view.PhotoGraphedProfi
 /**
  * Created by abdalla_maged on 10/1/2018.
  */
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
-    BottomNavigationView bottomNavigation;
+    private BottomAppBar bottomNavigation;
+    private Button homeBrn, campaignBtn, notificationBtn, myProfileBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,50 +33,76 @@ public class MainActivity extends BaseActivity {
         initView();
         initPresenter();
         initListener();
-
         getSupportFragmentManager().beginTransaction().replace(R.id.view_container, new HomeFragment()).commit(); //default Screen
-        bottomNavigation.setSelectedItemId(0);
     }
 
     @Override
     public void initView() {
         bottomNavigation = findViewById(R.id.bottom_navigation);
+        homeBrn = findViewById(R.id.navigation_home);
+        campaignBtn = findViewById(R.id.navigation_missions);
+        myProfileBtn = findViewById(R.id.navigation_profile);
+        notificationBtn = findViewById(R.id.navigation_notification);
+
     }
 
     @Override
     public void initPresenter() {
-
+        bottomNavigation.setOnClickListener(this);
+        homeBrn.setOnClickListener(this);
+        campaignBtn.setOnClickListener(this);
+        myProfileBtn.setOnClickListener(this);
+        notificationBtn.setOnClickListener(this);
     }
 
     private void initListener() {
 
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                switch (menuItem.getItemId()) {
-
-                    case R.id.navigation_home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.view_container, new HomeFragment()).commit();
-                        break;
-
-                    case R.id.navigation_missions:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.view_container, new CampaignsFragment()).commit();
-                        break;
-                    case R.id.navigation_profile:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.view_container, new PhotoGraphedProfileFragment()).commit();
-                        break;
-                    default:
-                        return false;
-                }
-                return true;
-            }
-
-        });
     }
 
     @Override
     public void showToast(String msg) {
         super.showToast(msg);
+    }
+
+    @Override
+    public void onClick(View v) {
+        clearSelected();
+        switch (v.getId()) {
+
+            case R.id.navigation_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.view_container, new HomeFragment()).commit();
+                homeBrn.setBackground(getResources().getDrawable(R.drawable.navifation_bar_bottom_background));
+
+                homeBrn.setFocusableInTouchMode(true);
+                homeBrn.requestFocus();
+                break;
+            case R.id.navigation_missions:
+                getSupportFragmentManager().beginTransaction().replace(R.id.view_container, new CampaignsFragment()).commit();
+                campaignBtn.setBackground(getResources().getDrawable(R.drawable.navifation_bar_bottom_background));
+                campaignBtn.setFocusableInTouchMode(true);
+                campaignBtn.requestFocus();
+                break;
+            case R.id.navigation_notification:
+                notificationBtn.setBackground(getResources().getDrawable(R.drawable.navifation_bar_bottom_background));
+                notificationBtn.setFocusableInTouchMode(true);
+                notificationBtn.requestFocus();
+                break;
+            case R.id.navigation_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.view_container, new PhotoGraphedProfileFragment()).commit();
+                myProfileBtn.setBackground(getResources().getDrawable(R.drawable.navifation_bar_bottom_background));
+                myProfileBtn.setFocusableInTouchMode(true);
+                myProfileBtn.requestFocus();
+                break;
+            default:
+        }
+    }
+
+    private void clearSelected() {
+        homeBrn.setFocusableInTouchMode(false);
+        campaignBtn.setFocusableInTouchMode(false);
+        notificationBtn.setFocusableInTouchMode(false);
+        myProfileBtn.setFocusableInTouchMode(false);
+
     }
 }
