62f65a817f38bbdba971de91fd20661e3958d4b1
abdalla maged <amaged@softmills.com>
2018-11-14 19:46:07 +0200

    Saving searchTap phase 5

--- app/src/main/java/com/example/softmills/phlog/ui/social/view/adapter/SocialAdapter.java
+++ app/src/main/java/com/example/softmills/phlog/ui/social/view/adapter/SocialAdapter.java
@@ -20,6 +20,8 @@ import com.example.softmills.phlog.ui.social.model.SocialData;
 
 import java.util.List;
 
+import static com.example.softmills.phlog.Utiltes.Constants.ALBUM_DISPLAY_TYPE_4;
+import static com.example.softmills.phlog.Utiltes.Constants.BRAND_DISPLAY_TYPE_1;
 import static com.example.softmills.phlog.Utiltes.Constants.CAMPAIGN_DISPLAY_TYPE_1;
 import static com.example.softmills.phlog.Utiltes.Constants.ENTITY_ALBUM;
 import static com.example.softmills.phlog.Utiltes.Constants.ENTITY_BRAND;
@@ -68,6 +70,7 @@ public class SocialAdapter extends RecyclerView.Adapter<SocialAdapter.SocialView
                 break;
             }
             case ENTITY_ALBUM: {
+                bindAlbumEntity(socialDataList.get(i).entites.get(0),socialDataList.get(i).albumName, socialViewHolder);
                 break;
             }
             case ENTITY_IMAGE: {
@@ -75,6 +78,7 @@ public class SocialAdapter extends RecyclerView.Adapter<SocialAdapter.SocialView
                 break;
             }
             case ENTITY_BRAND: {
+                bindBrandEntity(socialDataList.get(i).entites.get(0), socialViewHolder);
                 break;
             }
 
@@ -91,7 +95,7 @@ public class SocialAdapter extends RecyclerView.Adapter<SocialAdapter.SocialView
 
     class SocialViewHolder extends RecyclerView.ViewHolder {
 
-        FrameLayout socialProfileType3, socialImageSliderType5, socialCampaignType1;
+        FrameLayout socialProfileType3, socialImageSliderType5, socialCampaignType1, socialBrandType1,socialAlbumType4;
 
         ImageView socialProfileIcon, socialProfileImg_1, socialProfileImg_2, socialProfileImg_3, socialProfileImg_4;
         TextView socialProfileFullName, socialProfileUserName;
@@ -104,6 +108,13 @@ public class SocialAdapter extends RecyclerView.Adapter<SocialAdapter.SocialView
         ImageView socialCampaignIcon, socialCampaignImg;
         TextView socialCampaignName, socialCampaignTitle, socialCampaignDayLeft;
         Button socialJoinCampaignBtn;
+        /////
+        ImageView socialBrandIconImg, socialBrandImg;
+        TextView socialBrandName, socialBrandFollowing;
+        Button followBrandBtn;
+        /////
+        ImageView socialAlbumIconImg,socialAlbum1,socialAlbum2,socialAlbum3;
+        TextView socialAlbumName,socialAlbumPhotosNumber;
 
 
         SocialViewHolder(View view) {
@@ -113,7 +124,9 @@ public class SocialAdapter extends RecyclerView.Adapter<SocialAdapter.SocialView
             socialProfileType3 = view.findViewById(R.id.social_profile_type_3);
             socialImageSliderType5 = view.findViewById(R.id.images_slider_type_5);
             socialCampaignType1 = view.findViewById(R.id.social_campaign_type_1);
-            ////profileItemView type_1
+            socialBrandType1 = view.findViewById(R.id.social_brand_type_1);
+            socialAlbumType4 = view.findViewById(R.id.social_album_type_4);
+            /////profileItemView type_1
             socialProfileIcon = view.findViewById(R.id.social_profile_icon_img);
             socialProfileFullName = view.findViewById(R.id.social_profile_full_name);
             socialProfileUserName = view.findViewById(R.id.social_profile_user_name);
@@ -122,21 +135,35 @@ public class SocialAdapter extends RecyclerView.Adapter<SocialAdapter.SocialView
             socialProfileImg_2 = view.findViewById(R.id.social_profile_img_2);
             socialProfileImg_3 = view.findViewById(R.id.social_profile_img_3);
             socialProfileImg_4 = view.findViewById(R.id.social_profile_img_4);
-
-
-            ////ImageSlider type_1
+            /////ImageSlider type_1
             socialImgSlideRv = view.findViewById(R.id.social_img_slider_rv);
             socialImfSliderIcon =view.findViewById(R.id.social_icon_img);
             socialImageName = view.findViewById(R.id.social_image_name);
-
-
-            ///CampainItemView
+            /////CampaignItemView
             socialCampaignIcon = view.findViewById(R.id.social_campaign_icon);
             socialCampaignName = view.findViewById(R.id.social_campaign_name);
             socialCampaignImg = view.findViewById(R.id.social_campaign_img);
             socialCampaignTitle = view.findViewById(R.id.social_campaign_title);
             socialCampaignDayLeft = view.findViewById(R.id.social_campaign_day_left);
             socialJoinCampaignBtn = view.findViewById(R.id.social_join_campaign_btn);
+            /////BrandItemView
+            socialBrandIconImg = view.findViewById(R.id.social_brand_icon_img);
+            socialBrandImg = view.findViewById(R.id.social_brand_img);
+            socialBrandName = view.findViewById(R.id.social_brand_name);
+            socialBrandFollowing = view.findViewById(R.id.social_brand_following);
+            followBrandBtn = view.findViewById(R.id.follow_brand);
+            /////BrandAlbumView
+            socialAlbumIconImg=view.findViewById(R.id.social_album_icon_img);
+            socialAlbumName=view.findViewById(R.id.social_album_name);
+            socialAlbumPhotosNumber=view.findViewById(R.id.social_album_photos_number);
+            socialAlbum1=view.findViewById(R.id.social_album_img_1);
+            socialAlbum2=view.findViewById(R.id.social_album_img_2);
+            socialAlbum3=view.findViewById(R.id.social_album_img_3);
+
+
+
+
+
 
         }
     }
@@ -223,7 +250,6 @@ public class SocialAdapter extends RecyclerView.Adapter<SocialAdapter.SocialView
             case CAMPAIGN_DISPLAY_TYPE_1:
                 socialViewHolder.socialCampaignType1.setVisibility(View.VISIBLE);
 
-
                 GlideApp.with(context)
                         .load(entite.thumbnail)
                         .placeholder(R.drawable.default_user_pic)
@@ -238,7 +264,7 @@ public class SocialAdapter extends RecyclerView.Adapter<SocialAdapter.SocialView
                         .error(R.drawable.default_photographer_profile)
                         .into(socialViewHolder.socialCampaignImg);
 
-                socialViewHolder.socialCampaignTitle.setText(entite.titleEn);
+                socialViewHolder.socialCampaignTitle.setText(entite.nameEn);
                 socialViewHolder.socialCampaignDayLeft.setText("day left here");
                 socialViewHolder.socialCampaignName.setText(entite.nameEn);
                 socialViewHolder.socialJoinCampaignBtn.setOnClickListener(v -> {
@@ -247,4 +273,76 @@ public class SocialAdapter extends RecyclerView.Adapter<SocialAdapter.SocialView
         }
 
     }
+
+    private void bindBrandEntity(Entite entite, SocialViewHolder socialViewHolder) {
+        switch (entite.displayType) {
+
+            case BRAND_DISPLAY_TYPE_1: {
+                socialViewHolder.socialBrandType1.setVisibility(View.VISIBLE);
+
+                GlideApp.with(context)
+                        .load(entite.thumbnail)
+                        .placeholder(R.drawable.default_user_pic)
+                        .error(R.drawable.default_user_pic)
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(socialViewHolder.socialBrandIconImg);
+
+                GlideApp.with(context)
+                        .load(entite.brandCoverimg)
+                        .centerCrop()
+                        .placeholder(R.drawable.default_photographer_profile)
+                        .error(R.drawable.default_photographer_profile)
+                        .into(socialViewHolder.socialBrandImg);
+
+                socialViewHolder.socialBrandName.setText(entite.nameEn);
+                socialViewHolder.socialBrandFollowing.setText(entite.numberOfFollowers);
+                socialViewHolder.followBrandBtn.setOnClickListener(v -> {
+
+                });
+                break;
+            }
+        }
+    }
+
+    private void bindAlbumEntity(Entite entite,String AlbumName, SocialViewHolder socialViewHolder) {
+        switch (entite.displayType) {
+
+            case ALBUM_DISPLAY_TYPE_4: {
+                socialViewHolder.socialAlbumType4.setVisibility(View.VISIBLE);
+
+                GlideApp.with(context)
+                        .load(entite.imgs.get(0))
+                        .placeholder(R.drawable.default_user_pic)
+                        .error(R.drawable.default_user_pic)
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(socialViewHolder.socialAlbumIconImg);
+
+                GlideApp.with(context)
+                        .load(entite.imgs.get(0))
+                        .centerCrop()
+                        .placeholder(R.drawable.default_photographer_profile)
+                        .error(R.drawable.default_photographer_profile)
+                        .into(socialViewHolder.socialAlbum1);
+
+                GlideApp.with(context)
+                        .load(entite.imgs.get(1))
+                        .centerCrop()
+                        .placeholder(R.drawable.default_photographer_profile)
+                        .error(R.drawable.default_photographer_profile)
+                        .into(socialViewHolder.socialAlbum2);
+
+                GlideApp.with(context)
+                        .load(entite.imgs.get(2))
+                        .centerCrop()
+                        .placeholder(R.drawable.default_photographer_profile)
+                        .error(R.drawable.default_photographer_profile)
+                        .into(socialViewHolder.socialAlbum3);
+
+                socialViewHolder.socialAlbumName.setText(AlbumName);
+                socialViewHolder.socialAlbumPhotosNumber.setText(new StringBuilder().append(entite.imgs.size()).append(" ").append("photo").toString());
+                break;
+            }
+        }
+    }
+
 }
