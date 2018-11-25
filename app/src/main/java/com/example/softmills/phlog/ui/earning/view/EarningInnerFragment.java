f685a2a5b67875704b5e547e34366237075b35ef
ddopik <ddopik.01@gmail.com>
2018-11-25 20:47:09 +0200

    Saving FragmentEarningInner layout

---
+++ app/src/main/java/com/example/softmills/phlog/ui/earning/view/EarningInnerFragment.java
@@ -0,0 +1,42 @@
+package com.example.softmills.phlog.ui.earning.view;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.softmills.phlog.R;
+import com.example.softmills.phlog.base.BaseFragment;
+
+/**
+ * Created by abdalla_maged On Nov,2018
+ */
+public class EarningInnerFragment extends BaseFragment {
+
+    private View mainView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mainView = inflater.inflate(R.layout.fragment_earning_inner, container, false);
+        return mainView;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    protected void initPresenter() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
