03b3838405482abe74ba563e5dcf420fa3c7f31c
Omar Bdreldin <obdreldin@softmills.com>
2019-03-07 16:44:32 +0200

    changes: - re-enabled social data request

--- app/src/main/java/com/example/softmills/phlog/ui/social/view/SocialFragment.java
+++ app/src/main/java/com/example/softmills/phlog/ui/social/view/SocialFragment.java
@@ -57,12 +57,12 @@ public class SocialFragment extends BaseFragment implements SocialFragmentView, 
         initPresenter();
         initViews();
         initListener();
-//        socialFragmentPresenter.getSocialData(true);
-        try {
-            Utilities.intializeData("https://www.gettyimages.com/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        socialFragmentPresenter.getSocialData(true);
+//        try {
+//            Utilities.intializeData("https://www.gettyimages.com/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
 
