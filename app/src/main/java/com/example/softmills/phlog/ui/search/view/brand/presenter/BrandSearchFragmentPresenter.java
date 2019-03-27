b8a2da5230581d82a3cff3bd7cdd7238b6190e0e
Omar Bdreldin <obdreldin@softmills.com>
2019-03-27 16:35:49 +0200

    changes: - layout edit in brand search item - layout edit in profile search item

--- app/src/main/java/com/example/softmills/phlog/ui/search/view/brand/presenter/BrandSearchFragmentPresenter.java
+++ app/src/main/java/com/example/softmills/phlog/ui/search/view/brand/presenter/BrandSearchFragmentPresenter.java
@@ -1,5 +1,9 @@
 package com.example.softmills.phlog.ui.search.view.brand.presenter;
 
+import com.example.softmills.phlog.base.commonmodel.Business;
+
+import io.reactivex.Observable;
+
 /**
  * Created by abdalla_maged on 10/31/2018.
  */
@@ -8,4 +12,7 @@ public interface BrandSearchFragmentPresenter {
      void getSearchBrand(String key,int page);
 
 
+     Observable<Boolean> unfollowBrand(Business business);
+
+     Observable<Boolean> followBrand(Business business);
 }
